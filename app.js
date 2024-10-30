751ae612d1c91afd37bb41997ccf475cc0998ce9
Muhammad Ali <alikhancss27@gmail.com>
2024-10-30 14:13:59 +0500

    change

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
 
 function randompasswordgenerator(){
-  var randomstring='asdfghjkl1234567890/mnbvcxz!@#$%&poiyyt6789jhgffdmnbvcxzaqwetyuiioASDEWRTYGFDDERTTTFVVCXTTUIKKIMKM'
+  var randomstring='asdfghjkl1234567890##@@mnbvcxz!@#$%&poiyyt6789jhgffdmnbvcxzaqwetyuiioASDEWRTYGFDDERTTTFVVCXTTUIKKIMKM'
   var randompassword=''
 for(i=0;i<7;i++){
-  var randomnum = Math.floor(Math.random()*randomstring.length-1)
+  var randomnum = Math.floor(Math.random()*randomstring.length)
     randompassword+= randomstring[randomnum] }
 document.getElementById("input-range").innerHTML= `<span>${randompassword}</span>`
 }
